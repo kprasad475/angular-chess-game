dfbad84cc945ab7ea10524a1e82d50013ff91165
Binshabs <binshabs567@gmail.com>
2024-07-31 16:25:57 +0530

    chess-game-initialisation

---
+++ src/app/chess-game/chess-game.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ChessGameComponent } from './chess-game.component';
+
+describe('ChessGameComponent', () => {
+  let component: ChessGameComponent;
+  let fixture: ComponentFixture<ChessGameComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ChessGameComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(ChessGameComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/chess-game/chess-game.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chess-game',
+  templateUrl: './chess-game.component.html',
+  styleUrl: './chess-game.component.css'
+})
+export class ChessGameComponent {
+
+}
